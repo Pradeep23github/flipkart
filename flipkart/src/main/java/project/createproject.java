4484b08a680238eb5868ce36de5239439c78e674
User-pc <User-pc@DESKTOP-CAAVCUL>
2023-09-29 01:22:54 -0700

    flipkart

---
+++ flipkart/src/main/java/project/createproject.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class createproject {
+@Test
+public void createlist() {
+	Reporter.log("project created", true);
+}
+	
+
+}
---
+++ flipkart/src/test/java/projectlist/projectdetail.java
@@ -0,0 +1,11 @@
+package projectlist;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class projectdetail {
+@Test
+public void projectsummary() {
+	Reporter.log("i created project",true);
+}
+}
